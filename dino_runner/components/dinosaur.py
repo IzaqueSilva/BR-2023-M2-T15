fb373b15fe198c6c2924ba6aeb28a8abea0cd446
IzaqueSilva <Izaque.Brito@fundacion-jala.org>
2023-06-20 23:59:55 -0300

    Aula_4

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -5,7 +5,7 @@ from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 X_POS = 80
 Y_POS = 310
-DUCK_POS = 340
+Y_POS_DUCK = 340
 JUMP_VEL = 8.5
 
 
@@ -25,7 +25,7 @@ class Dinosaur(Sprite):
         if self.dino_run:
             self.run()
         elif self.dino_jump:
-            self.jump()
+            self.jump() 
         elif self.dino_duck:
             self.duck()
 
@@ -33,15 +33,15 @@ class Dinosaur(Sprite):
             self.dino_jump = True
             self.dino_run = False
             self.dino_duck = False
-        elif user_input[pygame.K_DOWN] and not self.dino_jump:   
-            self.dino_duck = True
+        elif user_input[pygame.K_DOWN] and not self.dino_jump:
             self.dino_run = False
-            self.dino_jump = False
-        elif not self.dino_jump:
+            self.dino_jump = False 
+            self.dino_duck = True
+        elif not self.dino_jump and not self.dino_duck:
             self.dino_run = True
             self.dino_jump = False
             self.dino_duck = False
-           
+
         if self.step_index >= 10:
             self.step_index = 0
         
@@ -67,8 +67,9 @@ class Dinosaur(Sprite):
         self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = X_POS
-        self.dino_rect.y = DUCK_POS
+        self.dino_rect.y = Y_POS_DUCK
         self.step_index += 1
+        self.dino_duck = False
 
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,10 +1,8 @@
 import pygame
 import random
 
-from dino_runner.components.obstacles.cactus import SmallCactus, LageCactus
+from dino_runner.components.obstacles.cactus import SmallCactus
 from dino_runner.components.obstacles.bird import Bird
-from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS
-from dino_runner.utils.constants import BIRD
 
 
 class ObstacleManager:
@@ -12,21 +10,24 @@ class ObstacleManager:
         self.obstacles = []
 
     def update(self, game):
-        if len(self.obstacles) == 0:
-            if random.randint(0, 2) == 0: 
-                self.obstacles.append(SmallCactus(SMALL_CACTUS))
-            elif random.randint(0, 2) == 1:
-                self.obstacles.append(LageCactus(LARGE_CACTUS))
+        obstacle_type = [
+            SmallCactus(),
+            Bird(),
+        ]
 
-            elif random.randint(0, 2) == 2:
-                self.obstacles.append(Bird(BIRD))
+        if len(self.obstacles) == 0:
+            self.obstacles.append(obstacle_type[random.randint(0,1)])
 
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
                 pygame.time.delay(500)
                 game.playing = False
+                game.death_count += 1
                 break
+
+    def reset_obstacles(self):
+        self.obstacles = []
 
     def draw(self, screen):
         for obstacle in self.obstacles:
